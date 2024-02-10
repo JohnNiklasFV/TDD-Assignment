56be745de3c36492aeef60198ab21c2bba7c899d
JohnNiklasFV <jnvenaasen@uia.no>
2024-02-10 21:35:50 +0100

    test2

--- Test.cs
+++ Test.cs
@@ -1,26 +1,47 @@
-   
+
 int inch = 1;
 double cm = 2.54;
+
+
 
 double actual = inch * cm;
 double expected = 2.54;
 
-void Test (double expected, double actual)
+void Test(double expected, double actual)
 {
     if (expected == actual)
     {
-        
+
         Console.WriteLine("🟢 Test Passed");
-    }   
+    }
     else
     {
         Console.WriteLine("🔴 Test Failed");
     }
-    
+
 }
-Test( expected, actual);
+Test(expected, actual);
 
 
 
 
 
+
+double meters = cm / 100;
+double actual2 = inch * meters;
+double expected2 = 0.0254;
+
+void Test2(double expected2, double actual2)
+{
+    if (expected2 == actual2)
+    {
+
+        Console.WriteLine("🟢 Test Passed");
+    }
+    else
+    {
+        Console.WriteLine($"🔴 Test Failed, expected:{expected2} actual:{actual2}");
+    }
+
+}
+Test2(expected2, actual2);
