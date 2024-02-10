858e6ba650a085a7e5811635758751a782afb3ba
JohnNiklasFV <jnvenaasen@uia.no>
2024-02-10 21:42:06 +0100

    test3

--- Test.cs
+++ Test.cs
@@ -45,3 +45,25 @@ void Test2(double expected2, double actual2)
 
 }
 Test2(expected2, actual2);
+
+
+
+
+double milimeters = cm * 10;
+double expected3 = 25.4;
+double actual3 = inch * milimeters;
+
+void Test3(double expected3, double actual3)
+{
+    if (expected3 == actual3)
+    {
+
+        Console.WriteLine("🟢 Test Passed");
+    }
+    else
+    {
+        Console.WriteLine($"🔴 Test Failed, expected:{expected3} actual:{actual3}");
+    }
+
+}
+Test3(expected3, actual3);
